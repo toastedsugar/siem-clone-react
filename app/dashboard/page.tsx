538ec86746e0cb95beb268e4234a59cc7fd1e5ae
E-WASTE <you@example.com>
2024-02-27 16:46:59 -0500

    Creating data parserse

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,12 @@
 import LogItemContainer from "@/components/LogItemContainer";
 
-export default function Page(){
-    return(
-        <div className="flex flex-col gap-4">
-            <LogItemContainer>
-                Log 1
-            </LogItemContainer>
-            <LogItemContainer>
-                Log 2
-            </LogItemContainer>
-            <LogItemContainer>
-                Log 3
-            </LogItemContainer>
-            <LogItemContainer>
-                Log 4
-            </LogItemContainer>
-        </div>
-    )
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4">
+      <LogItemContainer>Main Dashboard</LogItemContainer>
+      <LogItemContainer>Service 1</LogItemContainer>
+      <LogItemContainer>Service 2</LogItemContainer>
+      <LogItemContainer>Service 3</LogItemContainer>
+    </div>
+  );
 }
